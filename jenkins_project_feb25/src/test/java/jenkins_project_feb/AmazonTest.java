3e532a0786b8e9442f92ae0b341eab5a65ec1712
vipen <vipen@Hi-Speed-S340>
2025-02-07 09:20:13 +0530

    Amazon commit

---
+++ jenkins_project_feb25/src/test/java/jenkins_project_feb/AmazonTest.java
@@ -0,0 +1,13 @@
+package jenkins_project_feb;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AmazonTest {
+
+	@Test
+	public void launch() {
+	 
+		Reporter.log("Amazon launched",true);
+	}
+}
